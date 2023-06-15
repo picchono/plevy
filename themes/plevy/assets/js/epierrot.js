1534dcd0152db821b2607dcd50a39e6f33de48fd
picchono <72742974+picchono@users.noreply.github.com>
2023-06-15 22:29:58 +0200

    launching permanent

--- themes/plevy/assets/js/epierrot.js
+++ themes/plevy/assets/js/epierrot.js
@@ -1,57 +1,82 @@
 /* switch burger */
 function switchBurger() {
-    if (document.getElementById("navbox-btn")) {
-        let element = document.getElementById("navbox-btn");
-        let target = document.getElementById('target');
-        element.addEventListener('click', () => {
-            element.classList.toggle('open');
-            target.classList.toggle('hidden');
-        })
-    }
+  if (document.getElementById("navbox-btn")) {
+    let element = document.getElementById("navbox-btn");
+    let target = document.getElementById('target');
+    element.addEventListener('click', () => {
+      element.classList.toggle('open');
+      target.classList.toggle('hidden');
+    })
+  }
 }
+
+/* place menu at the top when phone for muuri */
+var doit;
+window.onresize = function(){
+  clearTimeout(doit);
+  doit = setTimeout(resizedw, 500);
+};
+function resizedw(){
+    if (this.resizeTO) clearTimeout(this.resizeTO);
+  this.resizeTO = setTimeout(function () {
+    const vwid = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    var allItems = muugrid.getItems();
+      var toShow = [];
+      for (let i = 0; i < allItems.length; i++) {
+        var inqu = allItems[i]["_element"]["classList"];
+        if (Object.values(inqu).indexOf('navmenu') > -1) {
+          if (vwid < 768) muugrid.move(i, 0);
+          else muugrid.move(i, 3);
+          console.log(i);
+          break;
+        }
+      }
+  }, 1000);
+};
+
 
 /* image loading */
 function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (rect.top < (window.innerHeight || document.documentElement.clientHeight));
+  const rect = element.getBoundingClientRect();
+  return (rect.top < (window.innerHeight || document.documentElement.clientHeight));
 }
 
 function showPic() {
-    setTimeout(function () {
-        var elmt = document.getElementsByClassName('imag');
-        for (var count = 0; count < elmt.length; count++) {
-            if (isInViewport(elmt[count]) && (elmt[count].getAttribute("data-toset"))) {
-                elmt[count].src = elmt[count].dataset.toset;
-                delete elmt[count].dataset.toset;
-            };
-        };
-    }, 100);
+  setTimeout(function () {
+    var elmt = document.getElementsByClassName('imag');
+    for (var count = 0; count < elmt.length; count++) {
+      if (isInViewport(elmt[count]) && (elmt[count].getAttribute("data-toset"))) {
+        elmt[count].src = elmt[count].dataset.toset;
+        delete elmt[count].dataset.toset;
+      };
+    };
+  }, 100);
 };
 
 //headListes change background
 function chgmainbckg(bckimg) {
-    document.getElementById("mainbckgimg").classList.add("blac");
-    setTimeout(() => {
-        document.getElementById("mainbckgimg").src = bckimg;
-        document.getElementById("mainbckgimg").classList.add("opaci");
-        document.getElementById("mainbckgimg").classList.remove("blac");
-    }, 200);
+  document.getElementById("mainbckgimg").classList.add("blac");
+  setTimeout(() => {
+    document.getElementById("mainbckgimg").src = bckimg;
+    document.getElementById("mainbckgimg").classList.add("opaci");
+    document.getElementById("mainbckgimg").classList.remove("blac");
+  }, 200);
 };
 
 function returnmainbckg() {
-    document.getElementById("mainbckgimg").classList.add("blac");
-    setTimeout(() => {
-        document.getElementById("mainbckgimg").src = document.getElementById("mainbckgimg").getAttribute("data-ref");
-        document.getElementById("mainbckgimg").classList.remove("opaci");
-        document.getElementById("mainbckgimg").classList.remove("blac");
-    }, 200);
+  document.getElementById("mainbckgimg").classList.add("blac");
+  setTimeout(() => {
+    document.getElementById("mainbckgimg").src = document.getElementById("mainbckgimg").getAttribute("data-ref");
+    document.getElementById("mainbckgimg").classList.remove("opaci");
+    document.getElementById("mainbckgimg").classList.remove("blac");
+  }, 200);
 };
 
 //click copyright
 document.addEventListener('click', function (event) {
-    if (!event.target.matches('.credphoto')) return;
-    event.preventDefault();
-    window.open(event.target.getAttribute("data-target"), '_blank').focus();
+  if (!event.target.matches('.credphoto')) return;
+  event.preventDefault();
+  window.open(event.target.getAttribute("data-target"), '_blank').focus();
 }, false);
 
 /* IRREGULARITY 1
@@ -194,7 +219,7 @@ document.addEventListener('click', function (event) {
   
     });
   })();
-*/  
+*/
 
 
 /*
@@ -227,7 +252,7 @@ document.addEventListener('click', function (event) {
 
 
 window.addEventListener('scroll', function () {
-    showPic();
+  showPic();
 });
 
 showPic();
