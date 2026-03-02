187e35defe363c2fe88e8b6ca938f2236eacf2ee
picchono <72742974+picchono@users.noreply.github.com>
2026-03-02 20:52:36 +0100

    update tailwindcss

--- themes/plevy26_list/postcss.config.js
+++ themes/plevy26_list/postcss.config.js
@@ -1,13 +1,5 @@
-const themeDir = __dirname;
-
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: [
-    require('tailwindcss')(themeDir + '/tailwind.config.js'),
-    require('autoprefixer')({
-      path: [themeDir]
-    })
-  ]
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
 }
-
-module.exports = config
